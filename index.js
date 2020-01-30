80c7d5bdd5d70da8c01c24541f8a809f07f1f3c7
LoggeL <hyper.xjo@gmx.de>
2020-01-30 17:19:14 +0100

    added error catcher

--- index.js
+++ index.js
@@ -78,272 +78,278 @@ client.on('raw', async rawPacket => {
 
     console.log("Fetching data for " + users[userID])
 
-    const summonerName = users[userID]
-    const response = await axios("https://raw.githubusercontent.com/CommunityDragon/Data/master/patches.json", { json: true })
-    const season = response.data.patches[response.data.patches.length - 1].season
-    //const summonerName = 'DeliriousPlayer'
-
-    const champions = (await kayn.DDragon.Champion.listDataByIdWithParentAsId())
-        .data
-
-    const summoner = await kayn.Summoner.by.name(summonerName)
-    let game = {}
     try {
-        game = await kayn.CurrentGame.by.summonerID(summoner.id)
-    }
-    catch (e) {
-        return console.error(e)
-    }
 
-    if (!game.gameId || recents.includes(game.gameId)) return
-    recents.push(game.gameId)
+        const summonerName = users[userID]
+        const response = await axios("https://raw.githubusercontent.com/CommunityDragon/Data/master/patches.json", { json: true })
+        const season = response.data.patches[response.data.patches.length - 1].season
+        //const summonerName = 'DeliriousPlayer'
 
-    let allyTeam = []
-    let enemyTeam = []
-    //console.log(game)
-    const player = game.participants.find(p => p.summonerName == summonerName)
+        const champions = (await kayn.DDragon.Champion.listDataByIdWithParentAsId())
+            .data
 
-    let elosPromise = []
-
-    for (let i = 0; i < game.participants.length; i++) {
-        elosPromise.push(kayn.League.Entries.by.summonerID(
-            game.participants[i].summonerId
-        ))
-    }
-
-    let elos = await Promise.all(elosPromise)
-    for (let i = 0; i < game.participants.length; i++) {
-        let team = {}
-        console.log(game.participants[i].summonerName)
-        let elo = elos[i]
-        let Solo5x5 = elo.find(e => e.queueType == 'RANKED_SOLO_5x5')
-        if (Solo5x5) {
-            team.tier = Solo5x5.tier
-            team.rank = Solo5x5.rank || "?"
-            team.wins = Solo5x5.wins || 0
-            team.losses = Solo5x5.losses || 0
-            team.lp = Solo5x5.leaguePoints || 0
-        }
-        team.name = game.participants[i].summonerName
-        team.champion = champions[game.participants[i].championId].key
-
-        let config = {
-            queue: 420,
-            champion: game.participants[i].championId,
-            beginIndex: 0
-            //season: season
-        }
-        const summ = await kayn.Summoner.by.id(game.participants[i].summonerId)
-        let collector = 0
+        const summoner = await kayn.Summoner.by.name(summonerName)
+        let game = {}
         try {
-            let filteredMatches
-            do {
-                let matches = await kayn.Matchlist.by.accountID(summ.accountId).query(config)
-                filteredMatches = matches.matches.filter(m => m.season == season && m.champion == game.participants[i].championId)
-                config.beginIndex = config.beginIndex + 100
-                collector = collector + filteredMatches.length
-                if (filteredMatches.length < 100) break
-            }
-            while (filteredMatches.length > 100)
-
-            team.champGames = collector
+            game = await kayn.CurrentGame.by.summonerID(summoner.id)
         }
         catch (e) {
-            team.champGames = 0
+            return console.error(e)
         }
-        if (game.participants[i].teamId == player.teamId) {
-            allyTeam.push(team)
-        } else {
-            enemyTeam.push(team)
+
+        if (!game.gameId || recents.includes(game.gameId)) return
+        recents.push(game.gameId)
+
+        let allyTeam = []
+        let enemyTeam = []
+        //console.log(game)
+        const player = game.participants.find(p => p.summonerName == summonerName)
+
+        let elosPromise = []
+
+        for (let i = 0; i < game.participants.length; i++) {
+            elosPromise.push(kayn.League.Entries.by.summonerID(
+                game.participants[i].summonerId
+            ))
         }
-    }
 
-    //console.log(allyTeam)
-    //console.log(enemyTeam)
+        let elos = await Promise.all(elosPromise)
+        for (let i = 0; i < game.participants.length; i++) {
+            let team = {}
+            console.log(game.participants[i].summonerName)
+            let elo = elos[i]
+            let Solo5x5 = elo.find(e => e.queueType == 'RANKED_SOLO_5x5')
+            if (Solo5x5) {
+                team.tier = Solo5x5.tier
+                team.rank = Solo5x5.rank || "?"
+                team.wins = Solo5x5.wins || 0
+                team.losses = Solo5x5.losses || 0
+                team.lp = Solo5x5.leaguePoints || 0
+            }
+            team.name = game.participants[i].summonerName
+            team.champion = champions[game.participants[i].championId].key
 
+            let config = {
+                queue: 420,
+                champion: game.participants[i].championId,
+                beginIndex: 0
+                //season: season
+            }
+            const summ = await kayn.Summoner.by.id(game.participants[i].summonerId)
+            let collector = 0
+            try {
+                let filteredMatches
+                do {
+                    let matches = await kayn.Matchlist.by.accountID(summ.accountId).query(config)
+                    filteredMatches = matches.matches.filter(m => m.season == season && m.champion == game.participants[i].championId)
+                    config.beginIndex = config.beginIndex + 100
+                    collector = collector + filteredMatches.length
+                    if (filteredMatches.length < 100) break
+                }
+                while (filteredMatches.length > 100)
 
-    fs.writeFileSync('allyTeam.json', JSON.stringify(allyTeam))
-    fs.writeFileSync('enemyTeam.json', JSON.stringify(enemyTeam))
-
-    // const allyTeam = require('./allyTeam.json')
-    //const enemyTeam = require('./enemyTeam.json')
-    //const version = "9.23.1"
-
-
-    const canvas = Canvas.createCanvas(700, 300)
-    const ctx = canvas.getContext('2d')
-
-    ctx.textAlign = "center"
-    ctx.textBaseline = "middle"
-
-    const background = await Canvas.loadImage('./scoreboardatlas.png')
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
-    let activeUsers = []
-    let heroPic, eloPic
-
-    // Ally
-    for (i = 0; i < allyTeam.length; i++) {
-        console.log('Rendering row', i)
-
-        heroPic = await Canvas.loadImage(
-            `champions/${allyTeam[i].champion}.png`
-        )
-        ctx.drawImage(heroPic, 25, i * 49 + 50, 40, 40)
-
-        // Green outline
-        ctx.strokeStyle = '#5cb85c'
-        ctx.lineWidth = 2
-        for (discordId in users) {
-            if (users[discordId] == allyTeam[i].name) {
-                ctx.strokeStyle = '#ffff00'
-                activeUsers.push(discordId)
+                team.champGames = collector
+            }
+            catch (e) {
+                team.champGames = 0
+            }
+            if (game.participants[i].teamId == player.teamId) {
+                allyTeam.push(team)
+            } else {
+                enemyTeam.push(team)
             }
         }
 
+        //console.log(allyTeam)
+        //console.log(enemyTeam)
 
-        ctx.beginPath();
-        ctx.arc(45, i * 49 + 70, 20, 0, 2 * Math.PI);
-        ctx.stroke();
 
-        /*
-        ctx.font = '20px "Roboto"'
-        ctx.fillStyle = '#5cb85c'
-        ctx.fillText('Ranked Wins', 90, 33)
+        fs.writeFileSync('allyTeam.json', JSON.stringify(allyTeam))
+        fs.writeFileSync('enemyTeam.json', JSON.stringify(enemyTeam))
 
-        ctx.fillStyle = '#d9534f'
-        ctx.fillText('Ranked Losses', 240, 33)
+        // const allyTeam = require('./allyTeam.json')
+        //const enemyTeam = require('./enemyTeam.json')
+        //const version = "9.23.1"
 
-        ctx.fillStyle = '#ffffff'
-        ctx.fillText('Ranked games with champ', 410, 33)
-        */
 
-        if (allyTeam[i].rank) {
-            eloPic = await Canvas.loadImage(
-                `./ranked-emblems/${allyTeam[i].tier}.png`
+        const canvas = Canvas.createCanvas(700, 300)
+        const ctx = canvas.getContext('2d')
+
+        ctx.textAlign = "center"
+        ctx.textBaseline = "middle"
+
+        const background = await Canvas.loadImage('./scoreboardatlas.png')
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
+        let activeUsers = []
+        let heroPic, eloPic
+
+        // Ally
+        for (i = 0; i < allyTeam.length; i++) {
+            console.log('Rendering row', i)
+
+            heroPic = await Canvas.loadImage(
+                `champions/${allyTeam[i].champion}.png`
             )
+            ctx.drawImage(heroPic, 25, i * 49 + 50, 40, 40)
 
-            ctx.drawImage(eloPic, 515 - 350, i * 49 + 48, 45, 45)
-            ctx.font = 'bold 20px "Roboto"'
-            ctx.fillStyle = '#dddddd'
-            ctx.fillText(allyTeam[i].rank, 590 - 350, i * 49 + 65)
-            ctx.font = 'bold 15px "Roboto"'
-            ctx.fillText(allyTeam[i].lp + ' LP', 590 - 350, i * 49 + 85)
-
-
-            ctx.font = 'bold 15px Calibri'
-            const percent = Math.round(allyTeam[i].wins / (allyTeam[i].wins + allyTeam[i].losses) * 100) / 100
-            let color
-            if (percent > 0.5) {
-                color = Math.min(percent, 0.7) - 0.5
-                color = rgbToHex(255 * (0.2 - color) / 0.2, 255, 0)
+            // Green outline
+            ctx.strokeStyle = '#5cb85c'
+            ctx.lineWidth = 2
+            for (discordId in users) {
+                if (users[discordId] == allyTeam[i].name) {
+                    ctx.strokeStyle = '#ffff00'
+                    activeUsers.push(discordId)
+                }
             }
-            else {
-                color = 0.5 - Math.max(percent, 0.3)
-                color = rgbToHex(255, 255 * (0.2 - color) / 0.2, 0)
-            }
-            ctx.fillStyle = color
-            ctx.fillRect(433 - 350, i * 49 + 51, 54, 24)
-            ctx.font = '900 20px "Roboto"'
-
-            ctx.fillStyle = '#000000'
-            ctx.fillText(Math.round(percent * 100) + " %", 460 - 350, i * 49 + 63)
-
-            ctx.font = 'bold 15px "Roboto"'
-            ctx.fillStyle = '#ffffff'
-            ctx.fillText(allyTeam[i].wins + allyTeam[i].losses + " Games", 460 - 350, i * 49 + 86)
-
-            ctx.fillStyle = '#ffffff'
-
-            let champPercent = allyTeam[i].champGames / (allyTeam[i].losses + allyTeam[i].wins) * 100
-            ctx.fillText(Math.round(champPercent) + " %", 655 - 350, i * 49 + 65)
-            ctx.fillText(allyTeam[i].champGames, 655 - 350, i * 49 + 85)
-        } else {
-            ctx.font = '30px "Roboto"'
-            ctx.fillStyle = '#dddddd'
-            ctx.fillText('Unranked', 545 - 350, i * 49 + 75)
-        }
-    }
-
-    // Enemy
-    for (i = 0; i < enemyTeam.length; i++) {
-
-        heroPic = await Canvas.loadImage(
-            `champions/${enemyTeam[i].champion}.png`
-        )
-        ctx.drawImage(heroPic, 375, i * 49 + 50, 40, 40)
-
-        // Red outline
-        ctx.strokeStyle = '#d9534f'
-        ctx.lineWidth = 2
-        ctx.beginPath();
-        ctx.arc(395, i * 49 + 70, 20, 0, 2 * Math.PI);
-        ctx.stroke();
-
-        if (enemyTeam[i].rank) {
-            eloPic = await Canvas.loadImage(
-                `./ranked-emblems/${enemyTeam[i].tier}.png`
-            )
-
-            ctx.drawImage(eloPic, 515, i * 49 + 48, 45, 45)
-            ctx.font = 'bold 20px "Roboto"'
-            ctx.fillStyle = '#dddddd'
-            ctx.fillText(enemyTeam[i].rank, 590, i * 49 + 65)
-            ctx.font = 'bold 15px "Roboto"'
-            ctx.fillText(enemyTeam[i].lp + ' LP', 590, i * 49 + 85)
 
 
-            ctx.font = 'bold 15px Calibri'
-            const percent = Math.round(enemyTeam[i].wins / (enemyTeam[i].wins + enemyTeam[i].losses) * 100) / 100
-            let color
-            if (percent > 0.5) {
-                color = Math.min(percent, 0.7) - 0.5
-                color = rgbToHex(255 * (0.2 - color) / 0.2, 255, 0)
-            }
-            else {
-                color = 0.5 - Math.max(percent, 0.3)
-                color = rgbToHex(255, 255 * (0.2 - color) / 0.2, 0)
-            }
-            ctx.fillStyle = color
-            ctx.fillRect(433, i * 49 + 51, 54, 24)
-            ctx.font = '900 20px "Roboto"'
+            ctx.beginPath();
+            ctx.arc(45, i * 49 + 70, 20, 0, 2 * Math.PI);
+            ctx.stroke();
 
-            ctx.fillStyle = '#000000'
-            ctx.fillText(Math.round(percent * 100) + " %", 460, i * 49 + 63)
-
-            ctx.font = 'bold 15px "Roboto"'
-            ctx.fillStyle = '#ffffff'
-            ctx.fillText(enemyTeam[i].wins + enemyTeam[i].losses + " Games", 460, i * 49 + 86)
-
-
-            /*ctx.fillStyle = '#5cb85c'
-            ctx.fillText(enemyTeam[i].wins, 545, i * 49 + 82)
+            /*
+            ctx.font = '20px "Roboto"'
+            ctx.fillStyle = '#5cb85c'
+            ctx.fillText('Ranked Wins', 90, 33)
     
             ctx.fillStyle = '#d9534f'
-            ctx.fillText(enemyTeam[i].losses, 585, i * 49 + 82)
+            ctx.fillText('Ranked Losses', 240, 33)
+    
+            ctx.fillStyle = '#ffffff'
+            ctx.fillText('Ranked games with champ', 410, 33)
             */
 
-            ctx.fillStyle = '#ffffff'
+            if (allyTeam[i].rank) {
+                eloPic = await Canvas.loadImage(
+                    `./ranked-emblems/${allyTeam[i].tier}.png`
+                )
 
-            let champPercent = enemyTeam[i].champGames / (enemyTeam[i].losses + enemyTeam[i].wins) * 100
-            ctx.fillText(Math.round(champPercent) + " %", 655, i * 49 + 65)
-            ctx.fillText(enemyTeam[i].champGames, 655, i * 49 + 85)
-        } else {
-            ctx.font = '30px "Roboto"'
-            ctx.fillStyle = '#dddddd'
-            ctx.fillText('Unranked', 545, i * 49 + 75)
+                ctx.drawImage(eloPic, 515 - 350, i * 49 + 48, 45, 45)
+                ctx.font = 'bold 20px "Roboto"'
+                ctx.fillStyle = '#dddddd'
+                ctx.fillText(allyTeam[i].rank, 590 - 350, i * 49 + 65)
+                ctx.font = 'bold 15px "Roboto"'
+                ctx.fillText(allyTeam[i].lp + ' LP', 590 - 350, i * 49 + 85)
+
+
+                ctx.font = 'bold 15px Calibri'
+                const percent = Math.round(allyTeam[i].wins / (allyTeam[i].wins + allyTeam[i].losses) * 100) / 100
+                let color
+                if (percent > 0.5) {
+                    color = Math.min(percent, 0.7) - 0.5
+                    color = rgbToHex(255 * (0.2 - color) / 0.2, 255, 0)
+                }
+                else {
+                    color = 0.5 - Math.max(percent, 0.3)
+                    color = rgbToHex(255, 255 * (0.2 - color) / 0.2, 0)
+                }
+                ctx.fillStyle = color
+                ctx.fillRect(433 - 350, i * 49 + 51, 54, 24)
+                ctx.font = '900 20px "Roboto"'
+
+                ctx.fillStyle = '#000000'
+                ctx.fillText(Math.round(percent * 100) + " %", 460 - 350, i * 49 + 63)
+
+                ctx.font = 'bold 15px "Roboto"'
+                ctx.fillStyle = '#ffffff'
+                ctx.fillText(allyTeam[i].wins + allyTeam[i].losses + " Games", 460 - 350, i * 49 + 86)
+
+                ctx.fillStyle = '#ffffff'
+
+                let champPercent = allyTeam[i].champGames / (allyTeam[i].losses + allyTeam[i].wins) * 100
+                ctx.fillText(Math.round(champPercent) + " %", 655 - 350, i * 49 + 65)
+                ctx.fillText(allyTeam[i].champGames, 655 - 350, i * 49 + 85)
+            } else {
+                ctx.font = '30px "Roboto"'
+                ctx.fillStyle = '#dddddd'
+                ctx.fillText('Unranked', 545 - 350, i * 49 + 75)
+            }
         }
+
+        // Enemy
+        for (i = 0; i < enemyTeam.length; i++) {
+
+            heroPic = await Canvas.loadImage(
+                `champions/${enemyTeam[i].champion}.png`
+            )
+            ctx.drawImage(heroPic, 375, i * 49 + 50, 40, 40)
+
+            // Red outline
+            ctx.strokeStyle = '#d9534f'
+            ctx.lineWidth = 2
+            ctx.beginPath();
+            ctx.arc(395, i * 49 + 70, 20, 0, 2 * Math.PI);
+            ctx.stroke();
+
+            if (enemyTeam[i].rank) {
+                eloPic = await Canvas.loadImage(
+                    `./ranked-emblems/${enemyTeam[i].tier}.png`
+                )
+
+                ctx.drawImage(eloPic, 515, i * 49 + 48, 45, 45)
+                ctx.font = 'bold 20px "Roboto"'
+                ctx.fillStyle = '#dddddd'
+                ctx.fillText(enemyTeam[i].rank, 590, i * 49 + 65)
+                ctx.font = 'bold 15px "Roboto"'
+                ctx.fillText(enemyTeam[i].lp + ' LP', 590, i * 49 + 85)
+
+
+                ctx.font = 'bold 15px Calibri'
+                const percent = Math.round(enemyTeam[i].wins / (enemyTeam[i].wins + enemyTeam[i].losses) * 100) / 100
+                let color
+                if (percent > 0.5) {
+                    color = Math.min(percent, 0.7) - 0.5
+                    color = rgbToHex(255 * (0.2 - color) / 0.2, 255, 0)
+                }
+                else {
+                    color = 0.5 - Math.max(percent, 0.3)
+                    color = rgbToHex(255, 255 * (0.2 - color) / 0.2, 0)
+                }
+                ctx.fillStyle = color
+                ctx.fillRect(433, i * 49 + 51, 54, 24)
+                ctx.font = '900 20px "Roboto"'
+
+                ctx.fillStyle = '#000000'
+                ctx.fillText(Math.round(percent * 100) + " %", 460, i * 49 + 63)
+
+                ctx.font = 'bold 15px "Roboto"'
+                ctx.fillStyle = '#ffffff'
+                ctx.fillText(enemyTeam[i].wins + enemyTeam[i].losses + " Games", 460, i * 49 + 86)
+
+
+                /*ctx.fillStyle = '#5cb85c'
+                ctx.fillText(enemyTeam[i].wins, 545, i * 49 + 82)
+        
+                ctx.fillStyle = '#d9534f'
+                ctx.fillText(enemyTeam[i].losses, 585, i * 49 + 82)
+                */
+
+                ctx.fillStyle = '#ffffff'
+
+                let champPercent = enemyTeam[i].champGames / (enemyTeam[i].losses + enemyTeam[i].wins) * 100
+                ctx.fillText(Math.round(champPercent) + " %", 655, i * 49 + 65)
+                ctx.fillText(enemyTeam[i].champGames, 655, i * 49 + 85)
+            } else {
+                ctx.font = '30px "Roboto"'
+                ctx.fillStyle = '#dddddd'
+                ctx.fillText('Unranked', 545, i * 49 + 75)
+            }
+        }
+        fs.writeFileSync('img.png', canvas.toBuffer())
+
+        let mentionString = activeUsers.map(a => '<@' + a + '>').join(' ')
+        const attachment = new Discord.MessageAttachment()
+
+        client.channels.get('647831066228293632').send(mentionString, {
+            files: [{
+                attachment: canvas.toBuffer(),
+                name: 'image.png'
+            }]
+        })
     }
-    fs.writeFileSync('img.png', canvas.toBuffer())
-
-    let mentionString = activeUsers.map(a => '<@' + a + '>').join(' ')
-    const attachment = new Discord.MessageAttachment()
-
-    client.channels.get('647831066228293632').send(mentionString, {
-        files: [{
-            attachment: canvas.toBuffer(),
-            name: 'image.png'
-        }]
-    })
+    catch (e) {
+        client.channels.get('647831066228293632').send("Loggo you fucked up bcs:\n`" + e + '`')
+    }
 })
 
 process.on('unhandledRejection', (reason, p) => {
